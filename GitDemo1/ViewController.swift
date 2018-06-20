9f06b88b4f76db79e41812a55e25eb69bfbd024d
Kumar_Bunty <abhinay.mca09@gmail.com>
2018-06-20 13:06:00 +0530

    GitDemo1 First Commit

---
+++ GitDemo1/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GitDemo1
+//
+//  Created by Abhinay on 20/06/18.
+//  Copyright © 2018 ONS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initialSetting()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK:- Private Methods
+    fileprivate func initialSetting(){
+        
+        
+    }
+
+
+}
+
